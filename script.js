18feac2334cc6dffdc3587be7bac82e2a1ab7ebd
HurleySA <matheus.eduardo.silva@ccc.ufcg.edu.br>
2021-09-09 11:05:41 -0300

    Finalizando 1ª parte html/css/js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 const li_animais = [...document.querySelectorAll(".animais-lista li")];
 const desc_animais = [...document.querySelectorAll(".animais-descricao section")];
 
@@ -32,5 +42,14 @@ const modificaFaq = (index) =>{
 
 dt_faq.forEach((element, index) => {
     element.addEventListener('click', () => {modificaFaq(index)
+    })
+})
+
+const copy = document.querySelector(".copy");
+
+copy.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
     })
 })
